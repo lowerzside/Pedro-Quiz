8c2d776b4268cbd61255518b04e7ab65c2acfcc6
lowerzside <pedropaulo.manso@hotmail.com>
2017-01-05 18:22:32 -0200

    quiz
    quiz

---
+++ app/src/main/java/com/example/pedro/quiz/Activity2.java
@@ -0,0 +1,76 @@
+package com.example.pedro.quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Objects;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+import static com.example.pedro.quiz.R.id.question;
+
+public class Activity2 extends AppCompatActivity {
+    private Question QuestionLibrary = new Question();
+    private Button mButtonChoice1;
+    private Button mButtonChoice2;
+    private Button mButtonChoice3;
+    private TextView question;
+
+    private String mAnswer;
+    private int mQuestionNumber = 0;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        question = (TextView) findViewById(R.id.question);
+        mButtonChoice1 = (Button) findViewById(R.id.A1);
+        mButtonChoice2 = (Button) findViewById(R.id.A2);
+        mButtonChoice3 = (Button) findViewById(R.id.A3);
+        updateQuestion();
+
+        mButtonChoice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mButtonChoice1.getText() == mAnswer) {
+                    updateQuestion();
+                }
+            }
+        });
+
+        mButtonChoice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mButtonChoice2.getText() == mAnswer) {
+                    updateQuestion();
+                }
+            }
+        });
+
+        mButtonChoice3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mButtonChoice3.getText() == mAnswer) {
+                    updateQuestion();
+                }
+            }
+        });}
+
+    private void updateQuestion() {
+        question.setText(Question.getQuestion(mQuestionNumber));
+        mButtonChoice1.setText(Question.getChoice1(mQuestionNumber));
+        mButtonChoice2.setText(Question.getChoice2(mQuestionNumber));
+        mButtonChoice3.setText(Question.getChoice3(mQuestionNumber));
+
+        mAnswer = Question.getCorrectAnswer(mQuestionNumber);
+        mQuestionNumber++;
+    }
+}
+
+
